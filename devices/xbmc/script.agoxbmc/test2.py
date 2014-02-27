0bf22842f6bcadf5707fd3485c7bdd29e9b90686
Alexey Balekhov <a@balek.ru>
2014-02-27 15:22:51 +0400

    Remove trash

--- devices/xbmc/script.agoxbmc/test2.py
+++
@@ -1,7 +0,0 @@
-from agocontrol.connection import AgoConnection
-from agocontrol.inventory import AgoInventory, AgoInventoryDevice
-
-connection = AgoConnection('cr', 'agocontrol', 'letmein')
-connection.open()
-inventory = AgoInventory(connection)
-print inventory.rooms, inventory.devices
